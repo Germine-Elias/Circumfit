1141d849a313e0e13b3d603b38a4b6a55ee766aa
Germine <eliasgermine@gmail.com>
2025-01-19 13:49:33 +0100

    Corrige les chemins et configure le fichier Procfile

--- measure/app.py
+++ measure/app.py
@@ -213,11 +213,6 @@ def calculate_bri():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-#if __name__ == '__main__':
-#    app.run(debug=True, host='0.0.0.0', port=5000)
-
 if __name__ == '__main__':
-    # Ne démarre que si l'environnement n'est pas "production"
-    if os.getenv("FLASK_ENV") != "production":
-        app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
